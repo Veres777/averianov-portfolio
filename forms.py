9d22b98ddf0ab018ebf749959d39f5dce717546c
Veres777 <spatial_traveller@proton.me>
2025-03-23 21:13:18 +0100

    Upravený formulář s validací a skrytým výpisem zpráv

--- forms.py
+++ forms.py
@@ -1,9 +1,18 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, SubmitField
-from wtforms.validators import DataRequired, Email
+from wtforms.validators import DataRequired, Email, Length
 
 class ContactForm(FlaskForm):
-    name = StringField('Jméno', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    message = TextAreaField('Zpráva', validators=[DataRequired()])
-    submit = SubmitField('Odeslat')
+    name = StringField('Jméno', validators=[
+        DataRequired(message="Prosím, vyplňte své jméno."),
+        Length(min=2, message="Jméno musí mít alespoň 2 znaky.")
+    ])
+    email = StringField('E-mail', validators=[
+        DataRequired(message="Prosím, zadejte e-mail."),
+        Email(message="Zadejte platnou e-mailovou adresu.")
+    ])
+    message = TextAreaField('Zpráva', validators=[
+        DataRequired(message="Napište zprávu, prosím."),
+        Length(min=10, message="Zpráva musí mít alespoň 10 znaků.")
+    ])
+    submit = SubmitField('Odeslat zprávu')
